2a38a27df0490617f707544589f5096ac8766f35
witek <witekdev@gmail.com>
2015-03-16 21:10:05 +0200

    fix indent

--- theme/js/controllers.js
+++ theme/js/controllers.js
@@ -1,18 +1,16 @@
 // Get all Optimate data with angular treeview using traversal
 (function(){
-  "use strict";
-  var myApp = angular.module('myApp', ['angularTreeview']);
-  myApp.controller('myController',['$scope', '$http',
-    function($scope, $http){
-      $http.get('http://127.0.0.1:8100').success
-      (function(data)
-        {$scope.roleList = data;}
-      );
-
-      $scope.formData = {};
-      $scope.closeModal = function(){
-        $scope.modalShown = false;
+    "use strict";
+    var myApp = angular.module('myApp', ['angularTreeview']);
+    myApp.controller('myController',['$scope', '$http',
+        function($scope, $http) {
+            $http.get('http://127.0.0.1:8100').success(function(data) {
+                $scope.roleList = data;
+            });
+            $scope.formData = {};
+            $scope.closeModal = function() {
+                $scope.modalShown = false;
+            }
         }
-      }]
-    );
+    ]);
 })();
