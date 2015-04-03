5d2b79033451e1634097ebc573c9f036209c3327
witek <witekdev@gmail.com>
2015-04-03 21:12:34 +0200

    when opening projects, show them in alphabetical order and prevent adding of duplicates (opening same project twice or more times).

--- theme/js/controllers.js
+++ theme/js/controllers.js
@@ -18,27 +18,44 @@
     // upon selection from the user
     myApp.controller('treeviewController',['$scope', '$http',
         function ProjectList($scope, $http) {
-          $scope.loadProject = function () {
-            var id = $('#project-select').find(":selected").val()
-            var url = 'http://127.0.0.1:8100/projectview/' + id + '/'
-            var req = {
-                method: 'GET',
-                url: url,
+            // aux function - checks if object is already in list based on ID
+            function containsObject(obj, list) {
+                var i;
+                for (i = 0; i < list.length; i++) {
+                    if (list[i].ID === obj.ID) {
+                        return true;
+                    }
+                }
+                return false;
             }
-            $http(req).success(function(data) {
-                $scope.roleList.push(data[0]);
+            $scope.loadProject = function () {
+                var id = $('#project-select').find(":selected").val()
+                var url = 'http://127.0.0.1:8100/projectview/' + id + '/'
+                var req = {
+                    method: 'GET',
+                    url: url,
+                }
+                $http(req).success(function(data) {
+                    if (!(containsObject(data[0], $scope.roleList))) {
+                        // add latest select project, if not already in the list
+                        $scope.roleList.push(data[0]); 
+                        // sort alphabetically by project name
+                        $scope.roleList.sort(function(a, b) {
+                            var textA = a.Name.toUpperCase();
+                            var textB = b.Name.toUpperCase();
+                            return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+                        });
+                    }
+                });  
+            };
+            $scope.roleList = [];
+            $scope.formData = {};
+            $scope.closeModal = function() {
+                $scope.modalShown = false;
+            }
+            $( document ).on( "click", "#select-project-submit", function( e ) { 
+                $scope.loadProject();
             });
-
-          };
-          $scope.roleList = [];
-          $scope.formData = {};
-          $scope.closeModal = function() {
-              $scope.modalShown = false;
-          }
-          $( document ).on( "click", "#select-project-submit", function( e ) { 
-              $scope.loadProject();
-          });      
-
         }
     ]);
     // Angular function that retrieves the Client data from the server
