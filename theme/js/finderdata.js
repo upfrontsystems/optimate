b2ba94e749fa28d3ad9b5db9bb577b9ef0a81d04
witek <witekdev@gmail.com>
2015-05-07 22:03:17 +0200

    intial commit of reference widget with some default data from Plone - currently commented out on main index page, work in progress.

---
+++ theme/js/finderdata.js
@@ -0,0 +1,115 @@
+var finderdata = {
+    "http://localhost:8080/Plone": {
+        "parent_url": "",
+        "path": [
+            {"url": "http://localhost:8080/Plone",
+            "icon": null,
+            "title": "Home"}
+            ],
+        "upload_allowed": false,
+        "items": [
+            {
+                "description": "Congratulations! You have successfully installed Plone.",
+                "uid": "811793d984af4c2dbdf4db2080df1e27",
+                "title": "Welcome to Plone",
+                "url": "http://localhost:8080/Plone/front-page",
+                "is_folderish": false,
+                "portal_type": "Document",
+                "icon": null,
+                "id": "front-page",
+                "normalized_type": "document"
+            },
+            {
+                "description": "Site News",
+                "uid": "ad59bb63e1d648fea9ec04818289a271",
+                "title": "News",
+                "url": "http://localhost:8080/Plone/news",
+                "is_folderish": true, "portal_type": "Folder",
+                "icon": null,
+                "id": "news",
+                "normalized_type": "folder"
+            },
+            {
+                "description": "Site Events",
+                "uid": "5e745db171614119af59be55ab12c425",
+                "title": "Events",
+                "url": "http://localhost:8080/Plone/events",
+                "is_folderish": true, "portal_type": "Folder",
+                "icon": null,
+                "id": "events",
+                "normalized_type": "folder"
+            },
+            {
+                "description": "Site Users",
+                "uid": "47cb04f38c9345f9a634c6c529382688",
+                "title": "Users",
+                "url": "http://localhost:8080/Plone/Members",
+                "is_folderish": true, "portal_type": "Folder",
+                "icon": null,
+                "id": "Members",
+                "normalized_type": "folder"
+            },
+        ]
+    },
+    "http://localhost:8080/Plone/news": {
+        "parent_url": "http://localhost:8080/Plone",
+        "path": [
+            {"url": "http://localhost:8080/Plone", "title": "Home"},
+            {"url": "http://localhost:8080/Plone/news",
+"title": "News"}
+        ],
+        "upload_allowed": false,
+        "items": [
+            {
+            "uid": "52c5b15d54a943e53280c039022a193f",
+            "title": "Plone 2.0.4 Released",
+            "url": "http://localhost:8080/Plone/news/2.0.4",
+            "is_folderish": false, "portal_type": "News Item",
+            "icon": "",
+            "id": "2.0.4",
+            "normalized_type": "news-item"
+            },
+            {
+            "uid": "878aa250543011d123775e6705347213",
+            "title": "29 contributions sponsored by Google",
+            "url": "http://localhost:8080/Plone/news/29-contributions-sponsored-by-google",
+            "is_folderish": false, "portal_type": "News Item",
+            "icon": "",
+            "id": "29-contributions-sponsored-by-google",
+            "normalized_type": "news-item"
+            },
+        ]
+    },
+    "http://localhost:8080/Plone/events": {
+        "parent_url": "http://localhost:8080/Plone",
+        "path": [
+            {
+            "url": "http://localhost:8080/Plone",
+            "title": "Home"
+            },
+            {
+            "url": "http://localhost:8080/Plone/events",
+            "title": "Events"}
+            ],
+        "upload_allowed": false,
+        "items": [
+            {
+            "uid": "eb9480a74c375da85abada276b4dcf0c",
+            "title": "Community Events",
+            "url": "http://localhost:8080/Plone/events/community",
+            "is_folderish": true, "portal_type": "Folder",
+            "icon": "",
+            "id": "community",
+            "normalized_type": "folder"
+            },
+            {
+            "uid": "7d09a2c25a840b0b9798383947611466",
+            "title": "50th Plone Tune-Up Tshirt",
+            "url": "http://localhost:8080/Plone/events/50th-plone-tuneup-tshirt",
+            "is_folderish": false, "portal_type": "Image",
+            "icon": "png.png",
+            "id": "50th-plone-tuneup-tshirt",
+            "normalized_type": "image"}
+            ]
+    }
+}
