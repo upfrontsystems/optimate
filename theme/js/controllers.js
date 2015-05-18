7e4fb5ecb283ce6df9fe9eb87213ef2737856bf7
nvernooy <vernooy.n@gmail.com>
2015-05-18 11:00:00 +0200

    Working on drag and drop

--- theme/js/controllers.js
+++ theme/js/controllers.js
@@ -823,4 +823,53 @@ allControllers.controller('treeviewController', ['$http', '$scope', 'globalServe
             // set currentNode
             $rootScope.currentNode = selectedNode;
         };
+
+        // handling drag and drop
+        // ====================================================================
+        var dragSrcEl = null;
+        function handleDragStart(e) {
+            console.log("starting drag");
+            this.style.opacity = '0.4';  // this / e.target is the source node.
+
+            dragSrcEl = this;
+
+            e.dataTransfer.effectAllowed = 'move';
+            e.dataTransfer.setData('text/html', this.innerHTML);
+        }
+
+        function handleDrop(e) {
+          // this/e.target is current target element.
+
+          if (e.stopPropagation) {
+            e.stopPropagation(); // Stops some browsers from redirecting.
+          }
+
+          // Don't do anything if dropping the same column we're dragging.
+          if (dragSrcEl != this) {
+            // Set the source column's HTML to the HTML of the column we dropped on.
+            dragSrcEl.innerHTML = this.innerHTML;
+            this.innerHTML = e.dataTransfer.getData('text/html');
+          }
+
+          return false;
+        }
+
+        function handleDragOver(e) {
+          if (e.preventDefault) {
+            e.preventDefault(); // Necessary. Allows us to drop.
+          }
+
+          e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+
+          return false;
+        }
+
+        function handleDragEnter(e) {
+          // this / e.target is the current hover target.
+          this.classList.add('over');
+        }
+
+        function handleDragLeave(e) {
+          this.classList.remove('over');  // this / e.target is previous target element.
+        }
 }])
