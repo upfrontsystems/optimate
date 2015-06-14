fac9128246c23e9abcb15a393a1e6dce22db11bc
witek <witekdev@gmail.com>
2015-06-14 19:08:07 +0200

    render a pyramid report template to pdf

--- src/optimate.app/optimate/app/reports.py
+++ src/optimate.app/optimate/app/reports.py
@@ -1,11 +1,41 @@
 import os
+import time
+import datetime
+from email.Utils import formatdate
 from pyramid.view import view_config
 from pyramid.response import Response
+from pyramid.renderers import render
+from StringIO import StringIO
+from xhtml2pdf import pisa
 
 @view_config(route_name="projectbudget")
 def projectbudget(request):
-    _here = os.path.dirname(__file__)
-    _pdf = open(os.path.join(
-                 _here, 'static', 'sample.pdf')).read()
-    return Response(content_type='application/pdf',
-                            body=_pdf)
+    # TODO create html report
+
+    # render template
+    template_data = render('templates/projectbudgetreport.pt',{},
+                           request=request)
+    html = StringIO(template_data.encode('utf-8'))
+
+    # Generate the pdf
+    pdf = StringIO()
+    pisadoc = pisa.CreatePDF(html, pdf, raise_exception=False)
+    assert pdf.len != 0, 'Pisa PDF generation returned empty PDF!'
+    html.close()
+    pdfcontent = pdf.getvalue()
+    pdf.close()
+
+    filename = "project_budget"
+    now = datetime.datetime.now()
+    nice_filename = '%s_%s' % (filename, now.strftime('%Y%m%d'))
+    last_modified = formatdate(time.mktime(now.timetuple()))
+
+    response = Response(content_type='application/pdf',
+                        body=pdfcontent)
+    response.headers.add('Content-Disposition',
+                         "attachment; filename=%s.pdf" % nice_filename)
+    response.headers.add("Content-Length", str(len(pdfcontent)))
+    response.headers.add('Last-Modified', last_modified)
+    response.headers.add("Cache-Control", "no-store")
+    response.headers.add("Pragma", "no-cache")
+    return response
