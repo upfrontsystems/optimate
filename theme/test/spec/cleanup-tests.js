0b7030c41db744b24c70d72779837525c75c59e7
nvernooy <vernooy.n@gmail.com>
2015-11-30 04:04:49 +0200

    Added tests back

--- theme/test/spec/cleanup-tests.js
+++ theme/test/spec/cleanup-tests.js
@@ -21,72 +21,72 @@ describe('Clean up tests', function () {
         browser.driver.findElement(protractor.By.id("signinButton")).click();
     });
 
-    // it('should delete the added payment', function(){
-    //     browser.get('http://127.0.0.1:8000/#/payments');
-    //     // click the payment
-    //     element(by.repeater('obj in jsonpayments').row(0)).click();
-    //     // delete
-    //     element(by.css('nav ul li a i.fa-trash')).click();
-    //     element(by.buttonText('Delete')).click();
-    // });
+    it('should delete the added payment', function(){
+        browser.get('http://127.0.0.1:8000/#/payments');
+        // click the payment
+        element(by.repeater('obj in jsonpayments').row(0)).click();
+        // delete
+        element(by.css('nav ul li a i.fa-trash')).click();
+        element(by.buttonText('Delete')).click();
+    });
 
-    // it('should delete the added claim', function () {
-    //     browser.get('http://127.0.0.1:8000/#/claims');
-    //     // click the claim
-    //     element(by.repeater('obj in jsonclaims').row(0)).click();
-    //     // delete
-    //     element(by.css('nav ul li a i.fa-trash')).click();
-    //     element(by.buttonText('Delete')).click();
-    // });
+    it('should delete the added claim', function () {
+        browser.get('http://127.0.0.1:8000/#/claims');
+        // click the claim
+        element(by.repeater('obj in jsonclaims').row(0)).click();
+        // delete
+        element(by.css('nav ul li a i.fa-trash')).click();
+        element(by.buttonText('Delete')).click();
+    });
 
-    // it('should delete the added valuation', function () {
-    //     browser.get('http://127.0.0.1:8000/#/valuations');
-    //     element(by.repeater('obj in jsonvaluations').row(0)).click();
-    //     // delete
-    //     element(by.css('nav ul li a i.fa-trash')).click();
-    //     element(by.buttonText('Delete')).click();
-    // });
+    it('should delete the added valuation', function () {
+        browser.get('http://127.0.0.1:8000/#/valuations');
+        element(by.repeater('obj in jsonvaluations').row(0)).click();
+        // delete
+        element(by.css('nav ul li a i.fa-trash')).click();
+        element(by.buttonText('Delete')).click();
+    });
 
-    // it('should delete the added invoice', function () {
-    //     browser.get('http://127.0.0.1:8000/#/invoices');
-    //     // click the invoice
-    //     element(by.repeater('obj in jsoninvoices').row(0)).click();
-    //     // delete
-    //     element(by.css('nav ul li a i.fa-trash')).click();
-    //     element(by.buttonText('Delete')).click();
-    // });
+    it('should delete the added invoice', function () {
+        browser.get('http://127.0.0.1:8000/#/invoices');
+        // click the invoice
+        element(by.repeater('obj in jsoninvoices').row(0)).click();
+        // delete
+        element(by.css('nav ul li a i.fa-trash')).click();
+        element(by.buttonText('Delete')).click();
+    });
 
-    // it('should delete the added order', function () {
-    //     browser.get('http://127.0.0.1:8000/#/orders');
-    //     // select the order
-    //     element(by.repeater('obj in jsonorders').row(0)).click();
-    //     expect(element(by.css('nav ul li a i.fa-trash')).isDisplayed()).toBe(true);
-    //     element(by.css('nav ul li a i.fa-trash')).click();
+    it('should delete the added order', function () {
+        browser.get('http://127.0.0.1:8000/#/orders');
+        // select the order
+        element(by.repeater('obj in jsonorders').row(0)).click();
+        expect(element(by.css('nav ul li a i.fa-trash')).isDisplayed()).toBe(true);
+        element(by.css('nav ul li a i.fa-trash')).click();
 
-    //     // check the confirmation modal
-    //     expect(element(by.css('div.modal:nth-child(4) > div:nth-child(1) > div:nth-child(1)')).isDisplayed()).toBe(true);
-    //     element(by.css('div.modal:nth-child(4) > div:nth-child(1) > div:nth-child(1)')).element(by.buttonText('Delete')).click();
-    // });
+        // check the confirmation modal
+        expect(element(by.css('div.modal:nth-child(4) > div:nth-child(1) > div:nth-child(1)')).isDisplayed()).toBe(true);
+        element(by.css('div.modal:nth-child(4) > div:nth-child(1) > div:nth-child(1)')).element(by.buttonText('Delete')).click();
+    });
 
-    // it('should delete the added project', function () {
-    //     browser.get('http://127.0.0.1:8000/#/projects');
-    //     // select the project
-    //     element(by.css('nav ul li a i.fa-folder-open')).click();
-    //     browser.switchTo().activeElement().sendKeys('TestProject');
-    //     element.all(by.css('.ac-select-list ul li')).then(function(items) {
-    //       items[0].click();
-    //     });
-    //     element(by.id('left')).element(by.buttonText('TestProject')).click();
-    //     expect(element(by.css('nav ul li a i.fa-trash')).isDisplayed()).toBe(true);
-    //     element(by.css('nav ul li a i.fa-trash')).click();
+    it('should delete the added project', function () {
+        browser.get('http://127.0.0.1:8000/#/projects');
+        // select the project
+        element(by.css('nav ul li a i.fa-folder-open')).click();
+        browser.switchTo().activeElement().sendKeys('TestProject');
+        element.all(by.css('.ac-select-list ul li')).then(function(items) {
+          items[0].click();
+        });
+        element(by.id('left')).element(by.buttonText('TestProject')).click();
+        expect(element(by.css('nav ul li a i.fa-trash')).isDisplayed()).toBe(true);
+        element(by.css('nav ul li a i.fa-trash')).click();
 
-    //     // check the confirmation modal
-    //     expect(element(by.css('div.modal:nth-child(4) > div:nth-child(1) > div:nth-child(1)')).isDisplayed()).toBe(true);
-    //     element(by.css('div.modal:nth-child(4) > div:nth-child(1) > div:nth-child(1)')).element(by.buttonText('Delete')).click();
+        // check the confirmation modal
+        expect(element(by.css('div.modal:nth-child(4) > div:nth-child(1) > div:nth-child(1)')).isDisplayed()).toBe(true);
+        element(by.css('div.modal:nth-child(4) > div:nth-child(1) > div:nth-child(1)')).element(by.buttonText('Delete')).click();
 
-    //     // check the project has been deleted
-    //     expect(element(by.id('left')).element(by.buttonText('TestProject')).isPresent()).toBe(false);
-    // });
+        // check the project has been deleted
+        expect(element(by.id('left')).element(by.buttonText('TestProject')).isPresent()).toBe(false);
+    });
 
     it('should delete the added client', function(){
         browser.get('http://127.0.0.1:8000/#/clients');
